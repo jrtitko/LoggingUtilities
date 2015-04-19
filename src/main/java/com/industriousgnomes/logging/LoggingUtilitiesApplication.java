11a2391e8ae5474f85b2875b66b156ab38b379eb
J.R. Titko <jrtitko1@jrs-macbook-pro.attlocal.net>
2015-04-19 14:51:25 -0400

    Initial project creation

---
+++ src/main/java/com/industriousgnomes/logging/LoggingUtilitiesApplication.java
@@ -0,0 +1,12 @@
+package com.industriousgnomes.logging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggingUtilitiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoggingUtilitiesApplication.class, args);
+    }
+}
